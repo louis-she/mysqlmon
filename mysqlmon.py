92246aba24c3855cbb5648aa1455fb420838932b
louis <hotmailgotohel@gmail.com>
2014-10-08 16:54:23 +0800

    auto change master done

--- mysqlmon.py
+++ mysqlmon.py
@@ -32,7 +32,7 @@ MASTER = {
 dbconns = []
 
 def alert(msg, type):
-    print msg
+    print msg, type
     log(msg)
 
 def do(func, params, repeat_times = 0, exit = False):
@@ -59,16 +59,15 @@ def do(func, params, repeat_times = 0, exit = False):
                 sys.exit(0)
             else:
                 return False
-
     return ret
 
 def check_process():
     pass
 
-def slave_change_master(slavedb, master_host, master_port, master_logfile, master_logpos):
+def slave_change_master(slavedb, master_host, master_port, master_logfile, master_logpos, master_user, master_password):
     cursor = slavedb.cursor(MySQLdb.cursors.DictCursor)
     cursor.execute("stop slave;")
-    cursor.execute("change master to master_host='%s', master_user='repl', master_password='louispass', master_port=%s, master_log_file='%s', master_log_pos=%s;" % (master_host, master_port, master_logfile, master_logpos))
+    cursor.execute("change master to master_host='{host}', master_user='{user}', master_password='{password}', master_port={port}, master_log_file='{logfile}', master_log_pos={logpos};".format(host=master_host, user=master_user, password=master_password, port=master_port, logfile=master_logfile, logpos=master_logpos))
     cursor.execute("start slave;")
     return True
 
@@ -90,7 +89,9 @@ def iosql_thread(slavedb):
     iot_status = ret["Slave_IO_Running"]
     if sqlt_status != "Yes":
         raise Exception("slave sql thread is not yes")
+        return False
     if iot_status != "Yes":
+        return False
         raise Exception("slave io thread is not yes")
     return True
 
@@ -115,7 +116,7 @@ def same_logfile(slavedb):
     return master_log_file
 
 def log(msg):
-    nowtime = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+    nowtime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
     fh = open("./logfile", "a")
     fh.write("[%s] %s\n" % (nowtime, msg))
 
@@ -140,26 +141,31 @@ def change_master(slaves):
         if not setmaster or curpos < logpos:
             setmaster = slave
             curpos = logpos
-
+    if not setmaster:
+        raise Exception("None of the slaves can be raised as master")
+        return False
     bemasterdb = do(connect, setmaster, 3)
     master_logfile, master_logpos = do(slave_become_master, {"slavedb": bemasterdb})
     master_host, master_port = setmaster["host"], setmaster["port"]
     slaves.remove(setmaster)
     for slave in slaves:
         changeinfo = {
-            "slavedb": do(connect, slave, 3),
-            "master_host"   : master_host,
-            "master_port"   : master_port,
-            "master_logpos" : master_logpos,
-            "master_logfile": master_logfile,
+            "slavedb"        : do(connect, slave, 3),
+            "master_host"    : master_host,
+            "master_password": "repl",
+            "master_user"    : "repl",
+            "master_port"    : master_port,
+            "master_logpos"  : master_logpos,
+            "master_logfile" : master_logfile,
         }
         do(slave_change_master, changeinfo)
 
 def connect(host, user, passwd, port):
     try:
-        db = MySQLdb.connect(host=host, user=user, passwd=passwd, port=port)
+        db = MySQLdb.connect(host=host, user=user, passwd=passwd, port=port, connect_timeout=1)
     except Exception, e:
-        raise Exception(e.args[1])
+        raise Exception("{host} {port} {sql_err}"\
+        .format(host=host, port=port, sql_err=e.args[1]))
     dbconns.append(db)
     return db
 
@@ -179,7 +185,7 @@ def cleardb():
 def master_routine(master):
     db = do(connect, master, 5)
     if not db:
-        change_master(SLAVES)
+        do(change_master, {"slaves": SLAVES})
 
 if __name__ == "__main__":
     master_routine(MASTER)
