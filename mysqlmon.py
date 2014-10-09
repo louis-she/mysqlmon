02a776bd131fcd94ce3b3e606125d7b1ebac7a5a
louis <hotmailgotohel@gmail.com>
2014-10-09 12:11:26 +0800

    ignore mysql warnings, next add daemon

--- mysqlmon.py
+++ mysqlmon.py
@@ -6,34 +6,55 @@ import os
 import time
 import sys
 import pycurl
+import threading
+from warnings import filterwarnings
 
-SLAVES = [
+filterwarnings('ignore', category = MySQLdb.Warning)
+
+test_dbs = [
     {
-    "host":"115.28.6.205",
-    "port":3312,
-    "user":"test",
-    "passwd":"2503",
+        "slaves": [
+            {
+            "host":"115.28.6.205",
+            "port":3312,
+            "user":"test",
+            "passwd":"2503",
+            },
+            {
+            "host":"115.28.6.205",
+            "port":3313,
+            "user":"test",
+            "passwd":"2503",
+            },
+        ],
+        "master": {
+            "host":"115.28.6.205",
+            "port":3311,
+            "user":"test",
+            "passwd":"2503",
+        }
     },
-    {
-    "host":"115.28.6.205",
-    "port":3313,
-    "user":"test",
-    "passwd":"2503",
-    },
+    # add more
 ]
 
-MASTER = {
-    "host":"115.28.6.205",
-    "port":3311,
-    "user":"test",
-    "passwd":"2503",
-}
-
-dbconns = []
+tglobal = threading.local()
 
 def alert(msg, type):
     print msg, type
     log(msg)
+
+
+class Single(threading.Thread):
+
+    def __init__(self, routine, suit = {}, globalvars = {}):
+        threading.Thread.__init__(self)
+        self._routine = routine
+        self._suit = suit
+
+    def run(self):
+        tglobal.dbconns = []
+        tglobal.suit = self._suit
+        self._routine()
 
 def do(func, params, repeat_times = 0, doalert=True):
     time.sleep(0.5)
@@ -62,10 +83,15 @@ def do(func, params, repeat_times = 0, doalert=True):
 def check_process():
     pass
 
-def slave_change_master(slavedb, master_host, master_port, master_logfile, master_logpos, master_user, master_password):
+def slave_change_master(slavedb, master_host, master_port, 
+                master_logfile, master_logpos, master_user, master_password):
     cursor = slavedb.cursor(MySQLdb.cursors.DictCursor)
     cursor.execute("stop slave;")
-    cursor.execute("change master to master_host='{host}', master_user='{user}', master_password='{password}', master_port={port}, master_log_file='{logfile}', master_log_pos={logpos};".format(host=master_host, user=master_user, password=master_password, port=master_port, logfile=master_logfile, logpos=master_logpos))
+    cursor.execute("change master to master_host='{host}', master_user='{user}',\
+master_password='{password}', master_port={port}, master_log_file='{logfile}', \
+master_log_pos={logpos};".format(host=master_host, user=master_user, \
+    password=master_password, port=master_port,logfile=master_logfile,\
+    logpos=master_logpos))
     cursor.execute("start slave;")
     return True
 
@@ -86,9 +112,11 @@ def iosql_thread(slavedb, host='', port=''):
     sqlt_status = ret["Slave_SQL_Running"]
     iot_status = ret["Slave_IO_Running"]
     if sqlt_status != "Yes":
-        raise Exception("{host} {port} slave sql thread is not yes".format(host=host, port=port))
+        raise Exception("{host} {port} slave sql thread is not yes"\
+              .format(host=host, port=port))
     if iot_status != "Yes":
-        raise Exception("{host} {port} slave io thread is not yes".format(host=host, port=port))
+        raise Exception("{host} {port} slave io thread is not yes"\
+              .format(host=host, port=port))
     return True
 
 def repl_delay(slavedb, host='', port=''):
@@ -138,7 +166,8 @@ def change_master(slaves):
             setmaster = slave
             curpos = logpos
     if not setmaster:
-        raise Exception("None of the slaves can be raised as master")
+        # raise Exception("None of the slaves can be raised as master") # disaster, no need to alert any more
+        return False
     bemasterdb = do(connect, setmaster, 3)
     master_logfile, master_logpos = do(slave_become_master, {"slavedb": bemasterdb})
     master_host, master_port = setmaster["host"], setmaster["port"]
@@ -153,7 +182,7 @@ def change_master(slaves):
             "master_logpos"  : master_logpos,
             "master_logfile" : master_logfile,
         }
-        do(slave_change_master, changeinfo)
+        do(slave_change_master, changeinfo, 0, False)
 
 def connect(host, user, passwd, port):
     try:
@@ -161,35 +190,33 @@ def connect(host, user, passwd, port):
     except Exception, e:
         raise Exception("{host} {port} {sql_err}"\
         .format(host=host, port=port, sql_err=e.args[1]))
-    dbconns.append(db)
+    tglobal.dbconns.append(db)
     return db
 
 def cleardb():
-    while dbconns:
-        dbconns.pop().close()
+    while tglobal.dbconns:
+        tglobal.dbconns.pop().close()
 
 def slave_routine(slave):
     slavedb = do(connect, slave, 5)
     if slavedb == False:
         return
-    ret = do(iosql_thread, 
-            {"slavedb" : slavedb, 
-             "host"    : slave["host"], 
-             "port"    : slave["port"]})
+    ret = do(iosql_thread, {"slavedb": slavedb, "host": slave["host"], "port": slave["port"]})
     if ret == False:
         return
-    do(repl_delay, 
-      {"slavedb": slavedb, 
-       "host"   : slave["host"], 
-       "port"   : slave["port"]})
+    do(repl_delay, {"slavedb": slavedb, "host": slave["host"], "port": slave["port"]})
 
 def master_routine(master):
     db = do(connect, master, 5)
     if not db:
-        do(change_master, {"slaves": SLAVES})
+        do(change_master, {"slaves": tglobal.suit["slaves"]})
+
+def suit_routine():
+    master_routine(tglobal.suit["master"])
+    for slave in tglobal.suit["slaves"]:
+        slave_routine(slave)
 
 if __name__ == "__main__":
-    master_routine(MASTER)
-    for slave in SLAVES:
-        slave_routine(slave)
-    cleardb()
+    for suit in test_dbs:
+        thread = Single(suit_routine, suit)
+        thread.start()
